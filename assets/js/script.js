fe3d660990578e05ab8cb96517fd27863945c84e
Maxwell Brown <mbrownet@gmail.com>
2023-02-09 14:55:19 -0500

    allowed for custom tasks and types to be submitted

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,25 @@ const tasksToDoEl = document.querySelector("#tasks-to-do");
 const createTaskHandler = () =>
 {
     event.preventDefault();
-    
+
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+
+    // give it a class name
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.append(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.append(listItemEl);
 };
 
